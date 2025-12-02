c54bc154844eb99d7347d1a6ae3625d97df58c02
groot <282764407@qq.com>
2025-12-02 16:01:14 +0800

    update bytesconv

--- pkg/bytesconv/bytesconv.go
+++ pkg/bytesconv/bytesconv.go
@@ -3,11 +3,12 @@ package bytesconv
 import "unsafe"
 
 func StringToBytes(s string) []byte {
-	x := (*[2]uintptr)(unsafe.Pointer(&s))
-	h := [3]uintptr{x[0], x[1], x[1]}
-	return *(*[]byte)(unsafe.Pointer(&h))
+	if s == "" {
+		return nil
+	}
+	return unsafe.Slice(unsafe.StringData(s), len(s))
 }
 
 func BytesToString(b []byte) string {
-	return *(*string)(unsafe.Pointer(&b))
+	return unsafe.String(unsafe.SliceData(b), len(b))
 }
